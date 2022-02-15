c00a165f8c7750c00fa8835ad6db2e1fb829dd07
Fran Escriche Grau <franchupinazo@hotmail.es>
2022-02-15 15:50:52 +0100

    Initial push
    Initial push

---
+++ Assets/Scripts/ScreenShotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ScreenShot();
+        }
+    }
+
+    public void ScreenShot()
+    {
+        Debug.Log("Kartoffel. Search in the route of the project");
+        ScreenCapture.CaptureScreenshot("Resume.png");
+    }
+}
